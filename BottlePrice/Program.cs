491c470f021d2415d1e92fbc2ceee534b1deb158
s0nlxaftrsh0ck <s0nlxaftrsh0ck@gmail.com>
2014-03-11 23:10:55 -0400

    First Commit of adding files

---
+++ BottlePrice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class BottlePrice
+{
+    static void Main()
+    {
+        string pricePerBottleString = Console.ReadLine();
+        int pricePerBottle = int.Parse(pricePerBottleString);
+
+        string tableCountString = Console.ReadLine();
+        int tableCount = int.Parse(tableCountString);
+
+        int bottleCount = ((tableCount + 99) / 100);
+
+        int salePrice = bottleCount * pricePerBottle;
+
+        Console.WriteLine ("The number of bottle is " +
+                                          bottleCount);
+        Console.WriteLine("The total price is " +
+                                            salePrice);
+    }
+}
